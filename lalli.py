161b8a044e79824613876fefa8a39eb95f5661fb
lalithareddyyy <121430784+lalithareddyyy@users.noreply.github.com>
2024-01-12 10:33:13 +0530

    Update lalli.py

--- lalli.py
+++ lalli.py
@@ -1,4 +1,4 @@
 import streamlit as st
-st.header('puppy')
-st.write('puppy')
+st.header('Dino')
+st.write('I'm a-roar-able dinosaur')
 st.image('https://img.freepik.com/premium-vector/cute-dinosaur-white-background_895166-19.jpg',caption='dino')
