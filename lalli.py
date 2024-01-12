7465c5c36db7bd20c36e2796850e840073dfcf8e
lalithareddyyy <121430784+lalithareddyyy@users.noreply.github.com>
2024-01-12 10:20:40 +0530

    Update lalli.py

--- lalli.py
+++ lalli.py
@@ -1,4 +1,4 @@
 import streamlit as st
 st.header('puppy')
 st.write('puppy')
-st.image('https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Fpremium-vector%2Fcute-dinosaur-white-background_46359036.htm&psig=AOvVaw01IJRBIGUPAIRA7MOsYDU4&ust=1705121126654000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCOjKvK2F14MDFQAAAAAdAAAAABAD',caption='dino')
+st.image('https://img.freepik.com/premium-vector/cute-dinosaur-white-background_895166-19.jpg',caption='dino')
